2469d9347f7a6ba5cf743b11740d442e1f34732f
Arya Dwi Putra <aryadptr.developer@gmail.com>
2024-11-29 10:58:31 +0700

    add create user api

--- app/api/v1/users/router.js
+++ app/api/v1/users/router.js
@@ -4,11 +4,16 @@ const express = require('express');
 //init express router
 const router = express.Router();
 
+const { validateUser } = require('../../../utils/validators/user');
+
 //import register controller
 const userController = require('../users/controller');
 
-//define route for login
+//define route for get all users
 router.get('/users', userController.findUsers);
+
+//define route for create user
+router.post('/users', validateUser, userController.createUser);
 
 //export router
 module.exports = router;
